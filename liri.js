09fa7a7e76160d4ebe093d8d8bdf2f8f6459b51e
Androw <androw@MacBook-Pro-2.local>
2018-11-12 21:16:50 -0500

    update

---
+++ liri.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+var Twitter = require("twitter");
+
+var spotify = require("node-spotify-api");
+
+var request = require("request");
+
+var fs = require("fs");
+
+var key = require("./key");
+
+var spotify = new Spotify(key.spotifyKey);
+
+var tweets = new twitter(key.twitterkey);
