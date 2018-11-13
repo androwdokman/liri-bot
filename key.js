495a5d0a206e23116b23b1bf172cddc6f87a8236
Androw <androw@MacBook-Pro-2.local>
2018-11-12 20:31:22 -0500

    update

--- key.js
+++ key.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    consumer_key: '3FTlgzf1QtqPPwGX9AxhTYpGi',
+    consumer_secret: 'gWDQQ5XfKanDlR2lkLisoclqst1D5R6WOaSDSB7hYGUZBGBI1Y',
+    access_token_key: '728243852918829056-FynAsO8w56Yqah1oIPJ68Tv1iMfp0BP',
+    access_token_secret: 'AUKV0LkNVJYspVanmJOu5Fj8Qp390aSkbVNrMX2OQ7844'
 };
 
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    id: 'df1e56de1780465e9bf8ab673939545a',
+    secret: '754d43c21a7a4739903bf7da75d8b7f6'
 };
